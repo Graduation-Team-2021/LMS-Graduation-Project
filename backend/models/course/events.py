0d7121a8e683076246940509500ead1537d86d1c
Adham Nour <adhamnourelwaffaa@gmail.com>
2021-01-22 14:40:39 +0200

    Events finished without upload

--- backend/models/course/events.py
+++ backend/models/course/events.py
@@ -13,6 +13,26 @@ class Events(db.Model,Base):
  event_date          =  db.Column(db.Date,nullable=False)
  course_code         =  db.Column(db.String(50), ForeignKey('course.course_code',ondelete='CASCADE',onupdate="CASCADE"), nullable=False)
  max_students        =  db.Column(db.Integer,nullable=False)
+ def serialize(self):
+     return{
+         "event_id": self.event_id,
+         "event_name": self.event_name,
+         "event_date": self.event_date,
+         "course_code": self.course_code,
+         "max_students":self.max_students
+     }
+ def insert(self):
+    db.session.add(self)
+    db.session.commit()
+  
+ def update(self):
+    db.session.merge(self)
+    db.session.commit()
+    
+
+ def delete(self):
+    db.session.delete(self)
+    db.session.commit()
 
 
  
