b56cffd735e7bbd47ddb45d5c1780d151b858ade
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-04-01 22:16:56 +0200

    working on mobile app - Login Screen

--- frontend/frontend-mob/App.js
+++ frontend/frontend-mob/App.js
@@ -1,21 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './screens/LoginScreen'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This is the corner stone of the application of LMS 2021</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screenView}>
+      <LoginScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screenView: {
     flex: 1,
-    backgroundColor: '#f00',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ frontend/frontend-mob/screens/LoginScreen.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+const LoginScren = (props) => {
+    return(
+        <View>
+            <Text>Getting Started</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default LoginScren;
