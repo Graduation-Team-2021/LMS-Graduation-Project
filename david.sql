0f08af083606f49af0cc3752c00226ac9148ee74
DJ-Man-2099 <davidmesak@hotmail.com>
2021-04-10 20:15:50 +0200

    Fixed the App Bar

--- david.sql
+++ david.sql
@@ -155,7 +155,7 @@ CREATE TABLE `group_project` (
 DROP TABLE IF EXISTS `learns`;
 CREATE TABLE `learns` (
   `student_id` int NOT NULL,
-  `course_code` varchar(5) NOT NULL,
+  `course_code` varchar(7) NOT NULL,
   PRIMARY KEY (`student_id`,`course_code`),
   KEY `course_code` (`course_code`),
   CONSTRAINT `learns_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -201,25 +201,29 @@ CREATE TABLE `post` (
   KEY `post_owner` (`post_owner`),
   CONSTRAINT `post_ibfk_1` FOREIGN KEY (`post_writer`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE,
   CONSTRAINT `post_ibfk_2` FOREIGN KEY (`post_owner`) REFERENCES `post_owner` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 DROP TABLE IF EXISTS `post_commenter`;
 CREATE TABLE `post_commenter` (
-  `commenter_id` int NOT NULL,
-  `post_id` int NOT NULL,
+  `comment_id` int NOT NULL AUTO_INCREMENT,
+  `commenter_id` int DEFAULT NULL,
+  `post_id` int DEFAULT NULL,
   `comment_text` mediumtext,
-  PRIMARY KEY (`commenter_id`,`post_id`),
+  `created_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`comment_id`),
+  KEY `commenter_id` (`commenter_id`),
   KEY `post_id` (`post_id`),
   CONSTRAINT `post_commenter_ibfk_1` FOREIGN KEY (`commenter_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE,
   CONSTRAINT `post_commenter_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 DROP TABLE IF EXISTS `post_liker`;
 CREATE TABLE `post_liker` (
   `liker_id` int NOT NULL,
   `post_id` int NOT NULL,
+  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`liker_id`,`post_id`),
   KEY `post_id` (`post_id`),
   CONSTRAINT `post_liker_ibfk_1` FOREIGN KEY (`liker_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE,
@@ -318,7 +322,7 @@ CREATE TABLE `student_questions` (
 DROP TABLE IF EXISTS `teaches`;
 CREATE TABLE `teaches` (
   `professor_id` int NOT NULL,
-  `course_code` varchar(5) NOT NULL,
+  `course_code` varchar(7) NOT NULL,
   PRIMARY KEY (`professor_id`,`course_code`),
   KEY `course_code` (`course_code`),
   CONSTRAINT `teaches_ibfk_1` FOREIGN KEY (`professor_id`) REFERENCES `professor` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -336,11 +340,11 @@ CREATE TABLE `user` (
   `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `national_id` (`national_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 
-INSERT INTO `course` (`course_code`,`course_name`,`weekly_hours`,`group_number`,`max_students`,`course_description`,`post_owner_id`) VALUES ('123','Networks',3,20,250,NULL,1),('CS88','NN',4,39,250,NULL,2),('CSE444','artificialIntelligenceAI',3,40,241,NULL,3);
+INSERT INTO `course` (`course_code`,`course_name`,`weekly_hours`,`group_number`,`max_students`,`course_description`,`post_owner_id`) VALUES ('123','Networks',3,20,250,NULL,1),('CS88','NN',4,39,250,NULL,2),('CSE4','artificialIntelligenceAI',3,40,241,NULL,3);
 
 
 INSERT INTO `deliverable` (`deliverable_id`,`deliverable_name`,`deadline`,`description`,`course_deliverables`,`students_number`,`mark`) VALUES (1,'MAnet','2021-01-01 00:00:00','nothing','123',290,15),(2,'Dijkstra','2020-05-01 00:00:00','niqwfe','123',29,20);
@@ -351,7 +355,7 @@ INSERT INTO `events` (`event_id`,`event_name`,`event_date`,`course_code`,`event_
 
 INSERT INTO `exams` (`exam_id`,`actual_mark`,`event_id`) VALUES (1,90,1),(5,85,3);
 
-INSERT INTO `finish` (`course_code`,`student_id`,`total_mark_in_the_course`) VALUES ('123',33,1500),('CS88',33,1500),('CSE444',44,1350);
+INSERT INTO `finish` (`course_code`,`student_id`,`total_mark_in_the_course`) VALUES ('123',33,1500),('CS88',33,1500),('CSE4',44,1350);
 
 
 INSERT INTO `group_project` (`group_id`,`group_name`,`group_description`,`post_owner_id`) VALUES (1,'TeamGrad','David we Zezo',4),(2,'Chicken Dinner',NULL,5);
@@ -361,15 +365,15 @@ INSERT INTO `learns` (`student_id`,`course_code`) VALUES (44,'CS88');
 INSERT INTO `material` (`material_id`,`material_type`,`material_name`,`course_material`) VALUES (1,'.png','Screenshot_20210327_180947','CS88'),(2,'.png','Screenshot_20210327_180947','CS88'),(3,'.png','Screenshot_20210327_180947','CS88'),(4,'.png','Screenshot_20210327_180947','CS88'),(5,'.png','Screenshot_20200317_223147','CS88'),(6,'.jpeg','1610321429147','CS88'),(7,'.pdf','Technical Writing','CS88');
 
 
-INSERT INTO `post` (`post_id`,`post_writer`,`post_owner`,`post_text`) VALUES (1,2,4,'1sst post'),(2,33,4,'2nd post'),(3,44,4,'test post'),(4,44,4,'Hey-O'),(5,44,4,'David'),(6,44,4,'DJ Man\n'),(7,44,2,'Hiiiii'),(8,44,2,'Hey-O'),(9,44,2,'What\'s Up???'),(10,44,NULL,'David');
+INSERT INTO `post` (`post_id`,`post_writer`,`post_owner`,`post_text`) VALUES (1,2,4,'1sst post'),(2,33,4,'2nd post'),(3,44,4,'test post'),(4,44,4,'Hey-O'),(5,44,4,'David'),(6,44,4,'DJ Man\n'),(7,44,2,'Hiiiii'),(8,44,2,'Hey-O'),(9,44,2,'What\'s Up???'),(10,44,NULL,'David'),(11,44,2,'Hey-oooooo'),(12,44,2,'What is going on'),(13,44,2,'Sonic'),(14,44,4,'Sonic'),(15,44,4,'Spider'),(16,44,4,'Mega'),(17,44,4,'Man'),(18,44,4,'Ben 10\n'),(19,44,4,'Zezoooooooooo');
 
-INSERT INTO `post_commenter` (`commenter_id`,`post_id`,`comment_text`) VALUES (5,1,'1st comment'),(5,2,'2nd comment for 1st user'),(33,1,'an added comment through post method');
+INSERT INTO `post_commenter` (`comment_id`,`commenter_id`,`post_id`,`comment_text`,`created_date`) VALUES (1,44,1,'dAVID','2021-04-08 18:56:50'),(2,44,1,'David','2021-04-08 18:57:02');
 
-INSERT INTO `post_liker` (`liker_id`,`post_id`) VALUES (5,1),(33,1),(43,1),(5,2);
+INSERT INTO `post_liker` (`liker_id`,`post_id`,`created_date`) VALUES (5,1,'2021-04-03 13:25:17'),(5,2,'2021-04-03 13:25:17'),(33,1,'2021-04-03 13:25:17'),(43,1,'2021-04-03 13:25:17'),(44,2,'2021-04-08 18:31:33'),(44,3,'2021-04-08 18:34:15'),(44,15,'2021-04-10 01:09:29');
 
 INSERT INTO `post_owner` (`id`) VALUES (1),(2),(3),(4),(5);
 
-INSERT INTO `professor` (`user_id`,`scientific_degree`) VALUES (5,'PHD doctorate');
+INSERT INTO `professor` (`user_id`,`scientific_degree`) VALUES (5,'PHD doctorate'),(52,''),(53,'');
 
 
 INSERT INTO `results` (`student_id`,`exam_id`,`mark`,`out_of_mark`) VALUES (33,1,65,90),(33,5,70,85);
@@ -380,9 +384,9 @@ INSERT INTO `student` (`user_id`,`student_year`) VALUES (33,4),(43,4),(44,3);
 INSERT INTO `student_group_relation` (`group_id`,`student_id`) VALUES (1,44);
 
 
-INSERT INTO `teaches` (`professor_id`,`course_code`) VALUES (5,'123'),(5,'CS88');
+INSERT INTO `teaches` (`professor_id`,`course_code`) VALUES (5,'123'),(5,'CS88'),(5,'CSE4');
 
-INSERT INTO `user` (`user_id`,`name`,`email`,`national_id`,`birthday`,`password`) VALUES (2,'Adham Nour','Adhoom@eng.asu.edu.eg','oig39inn','1999-02-02',NULL),(5,'Ashraf Salem','Ashroof@eng.com','3759uewfbi','1960-01-01',NULL),(33,'Hema Shoukry','hema@mam.com','zijwoewj','1990-01-01','komg'),(43,'Ziad Tarek','ziadtht@yahoo.com','poe932y','1950-01-01','pbkdf2:sha256:150000$oNidVhs0$fff38c1560d4cb688290161aa42e83c6751194b14af5d924a897d486b87867e5'),(44,'d','d','d','1998-07-24','pbkdf2:sha256:150000$6Jw6Sv6A$fca74764bfb559053a6310535ce30363fcc2ba81845410ef8b531a7cff2f2915'),(45,'evevw','ever','tg4t4','1999-01-01','pbkdf2:sha256:150000$82XOuxa3$7a79237615a505044d45ba1bb6c8e843eb7a54537fb04ab8abb7ee553a370fbc'),(47,'d','dd','dd','1999-01-01','pbkdf2:sha256:150000$ZgcLoTJh$185f4d5512f1c5a4a254cfc59ff255c71b8ad7554afd355c25422bd4e4f1212e');
+INSERT INTO `user` (`user_id`,`name`,`email`,`national_id`,`birthday`,`password`) VALUES (2,'Adham Nour','Adhoom@eng.asu.edu.eg','oig39inn','1999-02-02',NULL),(5,'Ashraf Salem','Ashroof@eng.com','3759uewfbi','1960-01-01',NULL),(33,'Hema Shoukry','hema@mam.com','zijwoewj','1990-01-01','komg'),(43,'Ziad Tarek','ziadtht@yahoo.com','poe932y','1950-01-01','pbkdf2:sha256:150000$oNidVhs0$fff38c1560d4cb688290161aa42e83c6751194b14af5d924a897d486b87867e5'),(44,'d','d','d','1998-07-24','pbkdf2:sha256:150000$6Jw6Sv6A$fca74764bfb559053a6310535ce30363fcc2ba81845410ef8b531a7cff2f2915'),(45,'evevw','ever','tg4t4','1999-01-01','pbkdf2:sha256:150000$82XOuxa3$7a79237615a505044d45ba1bb6c8e843eb7a54537fb04ab8abb7ee553a370fbc'),(47,'d','dd','dd','1999-01-01','pbkdf2:sha256:150000$ZgcLoTJh$185f4d5512f1c5a4a254cfc59ff255c71b8ad7554afd355c25422bd4e4f1212e'),(52,'David John','d@d.dd','12121212121212','2021-04-02','pbkdf2:sha256:150000$dKX3qmJF$7ac8b7cebb0b73a4c39a97d070e5d6059e98e3c260457bb2255d851eb8072d2c'),(53,'D','D@D.dj','14141141414114','2021-04-03','pbkdf2:sha256:150000$C3raZb82$46878b45714eaa16eb78fafccec804404e9980c2794e4fe4344b738f10e28f95');
 
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
