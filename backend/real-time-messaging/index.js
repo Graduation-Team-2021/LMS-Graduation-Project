23646da5a308e3ea56febf530584ca9717f6ba79
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-04-11 00:18:47 +0200

    finished the real time chatting server

--- backend/real-time-messaging/index.js
+++ backend/real-time-messaging/index.js
@@ -17,6 +17,32 @@ io.use((socket, next) => {
   next();
 });
 
-io.on("connection", (socket)=>{
-  
-})
+io.on("connection", (socket) => {
+  const users = [];
+  for (let [id, socket] of io.of("/").sockets) {
+    // this gets all connected sockets in the server
+    user.push({
+      socketID: id, //The ID of the user's socket
+      userID: socket.userId,
+    });
+  }
+  socket.emit("users", users);
+
+  socket.broadcast.emit("user connected", {
+    socketID: socket.id,
+    userID: socket.userId,
+  });
+  socket.on("private message", ({ content, to }) => {
+    socket.to(to).emit("private message", {
+      content,
+      from: socket.id,
+    });
+  });
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user disconnected", socket.id);
+  });
+});
+
+httpServer.listen(PORT, () =>
+  console.log(`server listening at http://localhost:${PORT}`)
+);
