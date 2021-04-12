38e4b5f08a0eb00ff1d09b03dd811a48f5d19148
DJ-Man-2099 <davidmesak@hotmail.com>
2021-04-12 16:57:49 +0200

    ada

--- frontend/frontend-web/src/Components/Messenger/Messenger.js
+++ frontend/frontend-web/src/Components/Messenger/Messenger.js
@@ -27,7 +27,11 @@ class Messenger extends Component {
   constructor(props) {
     super(props);
     console.log("connecting");
-    const socket = socetIO_Client("http://localhost:7000", {transports:['websocket'], autoConnect: false, upgrade:false });
+    const socket = socetIO_Client("http://localhost:7000", {
+      transports: ["websocket"],
+      autoConnect: false,
+      upgrade: false,
+    });
     socket.on("ServerAdmin", (msg) => console.log(msg));
     socket.connect();
     this.state = { Current: null, isNew: false };
