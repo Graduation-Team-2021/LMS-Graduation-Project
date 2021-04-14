fa7c990455dc69486da5e9a42071c45d6657865d
DJ-Man-2099 <davidmesak@hotmail.com>
2021-04-14 16:50:08 +0200

    Refactored Video v1

--- frontend/frontend-web/src/Ibrahim/Vedio.js
+++ frontend/frontend-web/src/Ibrahim/Vedio.js
@@ -1,24 +1,24 @@
 import React, { Component } from "react";
 import ReactPlayer from "react-player";
 //import Navbar from '../navbar/Navbar';
-import './Vedio.css';
+import "./Vedio.css";
 import { withRouter } from "react-router-dom";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
-import CoursePage from "../Containers/CoursePage/GroupPage"
+import CoursePage from "../Containers/CoursePage/GroupPage";
 
 class Vedioplayer extends Component {
   // axios.get()
 
   state = {
-    url: "https://www.youtube.com/watch?v=A3Ffwsnad0k&list=PLl-gb0E4MII28GykmtuBXNUNoej-vY5Rz&index=1"
-    
+    url:
+      "https://www.youtube.com/watch?v=A3Ffwsnad0k&list=PLl-gb0E4MII28GykmtuBXNUNoej-vY5Rz&index=1",
   };
 
   mathurl = () => {
     this.setState({
       url:
         // "https://youtu.be/8zuE3Rtm1Eo?list=PLQkyODvJ8ywuGxYwN0BfMSvembIJkNQH1"
-        "https://www.youtube.com/watch?v=rAxXcX_w5fE&list=PLl-gb0E4MII28GykmtuBXNUNoej-vY5Rz&index=2"
+        "https://www.youtube.com/watch?v=rAxXcX_w5fE&list=PLl-gb0E4MII28GykmtuBXNUNoej-vY5Rz&index=2",
     });
     alert("math");
     console.log(this.state.url);
@@ -26,8 +26,7 @@ class Vedioplayer extends Component {
   Phyurl = () => {
     this.setState({
       url:
-         "https://www.youtube.com/watch?v=hWEZsyF3ZZc&list=PLl-gb0E4MII28GykmtuBXNUNoej-vY5Rz&index=3"
-        
+        "https://www.youtube.com/watch?v=hWEZsyF3ZZc&list=PLl-gb0E4MII28GykmtuBXNUNoej-vY5Rz&index=3",
     });
     alert("phy");
     console.log(this.state.url);
@@ -35,16 +34,19 @@ class Vedioplayer extends Component {
   compurl = () => {
     this.setState({
       url:
-         "https://www.youtube.com/watch?v=A2k3ulOJ3u4&list=PLl-gb0E4MII28GykmtuBXNUNoej-vY5Rz&index=4"
+        "https://www.youtube.com/watch?v=A2k3ulOJ3u4&list=PLl-gb0E4MII28GykmtuBXNUNoej-vY5Rz&index=4",
     });
     alert("comp");
     console.log(this.state.url);
   };
   softwre = () => {
-    setTimeout(this.setState({
-      url:
-        "https://www.youtube.com/watch?v=N2YHianzseI&list=PLl-gb0E4MII28GykmtuBXNUNoej-vY5Rz&index=5"
-    }),3000)
+    setTimeout(
+      this.setState({
+        url:
+          "https://www.youtube.com/watch?v=N2YHianzseI&list=PLl-gb0E4MII28GykmtuBXNUNoej-vY5Rz&index=5",
+      }),
+      3000
+    );
 
     alert("software");
     console.log(this.state.url);
@@ -52,28 +54,26 @@ class Vedioplayer extends Component {
   nulling = () => {
     // props.history.push('/Courses')
     <BrowserRouter>
-    <Route path="/Courses">
-    <CoursePage/>
-    </Route>
-    </BrowserRouter>
+      <Route path="/Courses">
+        <CoursePage />
+      </Route>
+    </BrowserRouter>;
   };
 
-  start=()=>{
-    alert("you can start now your vedio")
-  }
-  finish=()=>{
-    alert("you have finished your vedio keeo going")
-  }
-  change=()=>{
-    alert("you can start now a new your vedio")
-  }
+  start = () => {
+    alert("you can start now your vedio");
+  };
+  finish = () => {
+    alert("you have finished your vedio keeo going");
+  };
+  change = () => {
+    alert("you can start now a new your vedio");
+  };
   render() {
     return (
-      <div>
-          {/*<Navbar/>*/}
-          <div className= "container">
+      <div className="container">
         <div className="Vedio">
-           <ReactPlayer
+          <ReactPlayer
             controls
             width="1500px"
             height="600px"
@@ -82,7 +82,7 @@ class Vedioplayer extends Component {
             onEnded={this.finish}
             onchange={this.change}
             onError={this.nulling}
-          /> 
+          />
           <ul className="vediolist">
             <li>
               <p> Your courses vedio :</p>
@@ -105,7 +105,6 @@ class Vedioplayer extends Component {
           </ul>
         </div>
       </div>
-   </div>
     );
   }
 }
