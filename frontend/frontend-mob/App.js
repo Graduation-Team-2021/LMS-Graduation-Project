ce5e57cef184ef8bcc57e948277079d5c0d2bd00
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-04-20 01:00:35 +0200

    initializing stuff

--- frontend/frontend-mob/App.js
+++ frontend/frontend-mob/App.js
@@ -1,18 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from './screens/LoginScreen'
 
 export default function App() {
   return (
-    <View style={styles.screenView}>
-      <LoginScreen />
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  screenView: {
+  container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
