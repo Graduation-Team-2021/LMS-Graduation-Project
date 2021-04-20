db4ca60945da5ed5366e9a0e031528a15d930c01
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-04-20 04:36:33 +0200

    finished Login Screen

--- frontend/frontend-mob/App.js
+++ frontend/frontend-mob/App.js
@@ -1,21 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import LoginScreen from './screens/Login'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LoginScreen />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ frontend/frontend-mob/screens/Login.js
@@ -0,0 +1,112 @@
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, ScrollView, Dimensions } from "react-native";
+import { Input, Image, Button } from "react-native-elements";
+
+const LoginScreen = () => {
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [EmailError, setEmailError] = useState(false);
+  const [PasswordError, setPasswordError] = useState(false);
+
+  const [LandScape, setLandScape] = useState(
+    Dimensions.get("window").width > Dimensions.get("window").height
+  );
+  useEffect(() => {
+    const updateLayout = () => {
+      setLandScape(
+        Dimensions.get("window").width > Dimensions.get("window").height
+      );
+    };
+    Dimensions.addEventListener("change", updateLayout);
+    return () => {
+      Dimensions.removeEventListener("change", updateLayout);
+    };
+  });
+
+  let screenLayout = styles.screen;
+  if (LandScape) {
+    screenLayout = { ...screenLayout, ...styles.screenL };
+  } else {
+    screenLayout = { ...screenLayout, ...styles.screenP };
+  }
+  return (
+    <ScrollView>
+      <View style={screenLayout}>
+        <View
+          style={LandScape ? styles.imageContainerL : styles.imageContainerP}
+        >
+          <Image
+            style={styles.image}
+            source={{
+              uri: "https://images7.alphacoders.com/567/thumb-1920-567688.jpg",
+            }}
+          />
+        </View>
+        <View style={styles.inputsContainer}>
+          <Text style={styles.h1}>Getting Started</Text>
+          <Input placeholder="Emails..." label="Email" />
+          <Input placeholder="Password..." label="Password" />
+          <Button
+            title="Submit"
+            onPress={() => {
+              //TODO: Implement this when connecting to the server
+              console.log("[submeit button] would be impelemented");
+            }}
+            buttonStyle={styles.submitButton}
+            containerStyle={styles.submitButtonContiner}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  screenL: {
+    flexDirection: "row-reverse",
+    paddingTop: 50,
+  },
+  screenP: {
+    flexDirection: "column",
+    padding: 50,
+  },
+  imageContainerP: {
+    width: "100%",
+    height: 200,
+  },
+  imageContainerL: {
+    height: "100%",
+    width: 300,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 50,
+  },
+  h1: {
+    fontSize: 28,
+    paddingTop: 10,
+    color: "red",
+    fontWeight: "bold",
+  },
+  inputsContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+    flex: 1,
+  },
+  submitButton: {
+    width: 200 ,
+  },
+  submitButtonContiner: {
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+    textAlign: "center",
+  },
+});
+export default LoginScreen;
