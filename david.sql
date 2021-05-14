51df74b0f28ed08340369c5c3889b31f2fd34dec
DJ-Man-2099 <davidmesak@hotmail.com>
2021-05-15 01:27:13 +0200

    yguk\

--- david.sql
+++ david.sql
@@ -7,8 +7,7 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `lms` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
-USE `lms`;
+USE `sql4412495`;
 DROP TABLE IF EXISTS `Prerequiste`;
 CREATE TABLE `Prerequiste` (
   `course_code` varchar(5) NOT NULL,
