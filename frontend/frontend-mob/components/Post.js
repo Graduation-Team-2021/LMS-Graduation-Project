f5b5de23121578f84f718c37186088d05a8df6ae
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-05-14 22:39:02 +0200

    finished single course screen

--- frontend/frontend-mob/components/Post.js
+++ frontend/frontend-mob/components/Post.js
@@ -1,29 +1,43 @@
-import React from 'react';
-import { View , Text , Stylesheet , TextInput  } from 'react-native'
-import { Card, Button , Icon  } from "react-native-elements";
+import React from "react";
+import { View, Text, Stylesheet, TextInput } from "react-native";
+import { Card, Button, Icon } from "react-native-elements";
 const Post = () => {
-    return ( 
-        <Card wrapperStyle={{justifyContent:'center',alignItems:'center',alignContent:'stretch'}}>
-            <Text>post by : owener name </Text>
-            <Text> contant </Text>
-            <View style={{flexDirection:"row",justifyContent:'space-between',width:'60%',padding:15}}>
-            <Icon 
-            name="Like_Outlined"
-            type="antdesign"/>
-            <Button
-            title="like"
-            />
-            <Button
-            title= "comment"
-            />
-            </View>
-            <TextInput
-            placeholder= " enter your comment"
-            multiline
-            style={{borderWidth:2 ,width:"100%", height: 50}}
-            />
-        </Card> 
-    );
-}
- 
+  return (
+    <Card
+      wrapperStyle={{
+        justifyContent: "center",
+        alignItems: "center",
+        alignContent: "stretch",
+      }}
+    >
+      <Text>post by : owener name </Text>
+      <Text> contant </Text>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          width: "60%",
+          padding: 15,
+        }}
+      >
+        <Button
+          title="like"
+          icon={<Icon name="like2" type="antdesign" />}
+          type="clear"
+        />
+        <Button
+          title="comment"
+          icon={<Icon name="comment" type="FontAwesome" />}
+          type="clear"
+        />
+      </View>
+      <TextInput
+        placeholder=" enter your comment"
+        multiline
+        style={{ borderWidth: 2, width: "100%", height: 50 }}
+      />
+    </Card>
+  );
+};
+
 export default Post;
