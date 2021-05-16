b5fe7e461412aead524da12cee38954ebbda3e6a
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-05-16 02:45:19 +0200

    finished the course list final commit isa

--- frontend/frontend-mob/screens/CourseDescriptionScreen.js
+++ frontend/frontend-mob/screens/CourseDescriptionScreen.js
@@ -5,27 +5,34 @@ import { Button } from "react-native-elements";
 const CourseDerscerption = (props) => {
   const Course = props.navigation.getParam("Course");
   return (
-      <View style={{ flex: 1, justifyContent: "center" }}>
-        <Image
-          style={{ width: "100%", height: "70%" }}
-          //source={require($`props.image`)}
-          source={{
-            uri: Course.coursePicURI,
-          }}
-        />
+    <View style={{ flex: 1, justifyContent: "center" }}>
+      <Image
+        style={{ width: "100%", height: "40%" }}
+        //source={require($`props.image`)}
+        source={{
+          uri: Course.coursePicURI,
+        }}
+      />
+      <ScrollView>
         <Text style={{ padding: 20, fontSize: 24, textAlign: "center" }}>
           {Course.courseDescription}
         </Text>
-        <Text style={{ padding: 20, fontSize: 24, textAlign: "center" }}>
-          {Course.courseName}
-        </Text>
-        <Button
-          //title= "go to $`props.CourseName`"
-          title={`Go To ${Course.courseName}`}
-          color="#F0F8FF"
-          //onpress=() =>{}
-        />
-      </View>
+      </ScrollView>
+      <Text style={{ padding: 20, fontSize: 24, textAlign: "center" }}>
+        {Course.courseName}
+      </Text>
+      <Button
+        //title= "go to $`props.CourseName`"
+        title={`Go To ${Course.courseName}`}
+        color="#F0F8FF"
+        onPress={() => {
+          props.navigation.navigate({
+            routeName: "Course",
+            params: { courseName: Course.courseName, groupflag: false },
+          });
+        }}
+      />
+    </View>
   );
 };
 
