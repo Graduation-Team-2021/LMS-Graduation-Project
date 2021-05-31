faa06aa6fcc1099a88a549eaf4f69682c24cf4db
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-05-31 18:14:32 +0200

    finished the drawer

--- frontend/frontend-mob/components/Drawer.js
+++ frontend/frontend-mob/components/Drawer.js
@@ -3,63 +3,47 @@ import { View, Text, StyleSheet } from "react-native";
 import SafeAreaView from "react-native-safe-area-view";
 
 import { DrawerItems } from "react-navigation-drawer";
-import { LinearGradient } from "expo-linear-gradient";
+import Icon from "react-native-vector-icons/AntDesign";
 
-import { Avatar, Card } from "react-native-elements";
+import { Avatar, Button } from "react-native-elements";
 
 const Drawer = (props) => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View>
-        <View>
-          <Card containerStyle={styles.cardContainerStyle}>
-            <LinearGradient
-              // Button Linear Gradient
-              colors={["red", "blue"]}
-              style={styles.avatarBackground}
-              start={{
-                x: 0,
-                y: 0.5,
-              }}
-              end={{
-                x: 1,
-                y: 0.5,
-              }}
-            >
-              <Avatar
-                rounded
-                size="xlarge"
-                source={{
-                  uri: "https://avatarfiles.alphacoders.com/263/thumb-1920-263348.jpg",
-                }}
-                containerStyle={styles.avatarContainerStyle}
-              />
-            </LinearGradient>
-            <Card.Divider />
-            <View style={{ alignItems: "center", paddingBottom: 15 }}>
-              <Text>THe User Name</Text>
-              <Text>The user current year</Text>
-            </View>
-          </Card>
+        <View
+          style={{
+            paddingVertical: "7%",
+            flexDirection: "row",
+            alignItems: "center",
+            backgroundColor: "#ccc",
+          }}
+        >
+          <Avatar
+            rounded
+            size="large"
+            source={{
+              uri: "https://avatarfiles.alphacoders.com/263/thumb-1920-263348.jpg",
+            }}
+            containerStyle={styles.avatarContainerStyle}
+          />
+          <View>
+            <Text>The User Name</Text>
+            <Text>The User Current year</Text>
+          </View>
         </View>
         <DrawerItems {...props} />
+        <Button
+          icon={<Icon name="logout" size={15} color="white" style={{padding: 10}} />}
+          title="Logout"
+        />  
       </View>
+      
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    alignItems: "center",
-    padding: 30,
-  },
-  cardContainerStyle: {
-    borderRadius: 25,
-    width: "100%",
-    alignItems: "stretch",
-    padding: 0,
-  },
   avatarContainerStyle: {
     borderColor: "blueviolet",
     borderWidth: 5,
@@ -70,22 +54,6 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 25,
     justifyContent: "center",
     alignItems: "center",
-  },
-  displayDataCardWrapperStyle: {
-    alignItems: "center",
-    justifyContent: "center",
-    alignSelf: "center",
-  },
-  displayDataCardContainerStyle: {
-    borderRadius: 25,
-    width: "80%",
-  },
-  text: {
-    fontWeight: "bold",
-    fontSize: 20,
-  },
-  title: {
-    paddingTop: 40,
   },
 });
 
