7ea40f5532301ceca01af109f2168faf06593602
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-06-02 13:56:25 +0200

    added Deliverable submetion screen

--- frontend/frontend-mob/screens/DeliverableDescriptionScreen.js
+++ frontend/frontend-mob/screens/DeliverableDescriptionScreen.js
@@ -15,27 +15,33 @@ const DeliverableDescription = (props) => {
       />
       <ScrollView>
         <Text style={{ padding: 20, fontSize: 24, textAlign: "center" }}>
-          {"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor purus ut efficitur imperdiet."+
-           "Phasellus vel tortor sit amet ligula sollicitudin suscipit. Proin lobortis ut lorem ac tristique. Vivamus leo diam, "+
-           "dictum ac consectetur ut, mattis sit amet est. Proin at dui tincidunt, rutrum nulla sed, placerat augue."}
+          {"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor purus ut efficitur imperdiet." +
+            "Phasellus vel tortor sit amet ligula sollicitudin suscipit. Proin lobortis ut lorem ac tristique. Vivamus leo diam, " +
+            "dictum ac consectetur ut, mattis sit amet est. Proin at dui tincidunt, rutrum nulla sed, placerat augue."}
         </Text>
       </ScrollView>
       <Text style={{ padding: 20, fontSize: 24, textAlign: "center" }}>
         {Deliverable.name}
       </Text>
       <Text style={{ padding: 10, fontSize: 16, textAlign: "center" }}>
-        {(Deliverable.type==="Quiz")?(`Allowed time: ${Deliverable.leeway}`):null}
+        {Deliverable.type === "Quiz"
+          ? `Allowed time: ${Deliverable.leeway}`
+          : null}
       </Text>
       <Button
         //title= "go to $`props.CourseName`"
-        title={(Deliverable.type==="Quiz")?(`Begin ${Deliverable.name} ${Deliverable.type}`):(`Go to ${Deliverable.name} ${Deliverable.type}`)}
+        title={
+          Deliverable.type === "Quiz"
+            ? `Begin ${Deliverable.name} ${Deliverable.type}`
+            : `Go to ${Deliverable.name} ${Deliverable.type}`
+        }
         color="#F0F8FF"
-        /*onPress={() => {
+        onPress={() => {
           props.navigation.navigate({
-            routeName: "Course",
-            params: { deliverableName: Deliverable.name, groupflag: false },
+            routeName: "DeliverableSubmetion",
+            params: { delevrableName: Deliverable.name,deliverableType:Deliverable.type },
           });
-        }}*/
+        }}
       />
     </View>
   );
---
+++ frontend/frontend-mob/screens/DeliverableSubmetionScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+const QuizScreen = (props) => {
+    return ( <View><Text>This is a Quiz</Text></View> );
+}
+ 
+export default QuizScreen;
