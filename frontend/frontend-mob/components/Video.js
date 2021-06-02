e51b6dd70d2ea79c41b5c90834f2c0101982bdc5
Michel <michelattallah123@gmail.com>
2021-06-02 20:33:56 +0200

    added video and pdf reader components

---
+++ frontend/frontend-mob/components/Video.js
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import { View, StyleSheet, Button, ProgressViewIOSComponent } from 'react-native';
+import { Video, AVPlaybackStatus } from 'expo-av';
+
+export default function Video(props) {
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
+  return (
+    <View style={styles.container}>
+      <Video
+        ref={video}
+        style={styles.video}
+        source={{
+          uri:props.videoFile ,
+        }}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+      />
+      <View style={styles.buttons}>
+        <Button
+          title={status.isPlaying ? 'Pause' : 'Play'}
+          onPress={() =>
+            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+          }
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  video: {
+    alignSelf: 'center',
+    width: 320,
+    height: 200,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
