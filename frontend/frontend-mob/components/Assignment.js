c3fa9d351796992f84f6667a97133966979df407
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-06-03 19:39:53 +0200

    finished assignment page

--- frontend/frontend-mob/components/Assignment.js
+++ frontend/frontend-mob/components/Assignment.js
@@ -6,46 +6,71 @@ import {
   Alert,
   StyleSheet,
   Dimensions,
+  ScrollView,
 } from "react-native";
 import { Card } from "react-native-elements";
 import * as DocumentPicker from "expo-document-picker";
+import BeingUploadedFile from "./BeingUploadedFile";
 
 const Assignment = (props) => {
   const [uploadedFiles, setUploadedFiles] = useState([]);
+  const deleteFile = (target_index) => {
+    let newUplodedFiles = uploadedFiles.filter(
+      (_, index) => index !== target_index
+    );
+    setUploadedFiles(newUplodedFiles);
+  };
 
   let renderdFile = <Text>There's no file to be uploaded</Text>;
   if (uploadedFiles.length > 0) {
     renderdFile = uploadedFiles.map((file, index) => (
-      <Text key={index}>{file.name}</Text>
+      <BeingUploadedFile
+        file={file}
+        key={index}
+        changeList={deleteFile}
+        index={index}
+      />
     ));
   }
 
   return (
     <Fragment>
-      <Card containerStyle={styles.cardContainerStyle}>{renderdFile}</Card>
-      <View style={{padding:15}}>
-        <Button
-          title="Pick a File to Upload"
-          onPress={() => {
-            DocumentPicker.getDocumentAsync().then((value) => {
-              if (value.type === "cancel") {
-                Alert.alert(
-                  "Picking file canceled",
-                  "you have canceled picking a file operation",
-                  [
-                    {
-                      text: "Ok",
-                      onPress: () => console.log("ok"),
-                      style: "default",
-                    },
-                  ]
-                );
-              } else {
-                setUploadedFiles([...uploadedFiles, value]);
-              }
-            });
-          }}
-        />
+      <Card containerStyle={styles.cardContainerStyle}>
+        <ScrollView>{renderdFile}</ScrollView>
+      </Card>
+      <View style={styles.buttonContainer}>
+        <View style={styles.button}>
+          <Button
+            title="Pick a File to Upload"
+            onPress={() => {
+              DocumentPicker.getDocumentAsync().then((value) => {
+                if (value.type === "cancel") {
+                  Alert.alert(
+                    "Picking file canceled",
+                    "you have canceled picking a file operation",
+                    [
+                      {
+                        text: "Ok",
+                        onPress: () => console.log("ok"),
+                        style: "default",
+                      },
+                    ]
+                  );
+                } else {
+                  setUploadedFiles([...uploadedFiles, value]);
+                }
+              });
+            }}
+          />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title="Upload"
+            onPress={() => {
+              console.log("Upload");
+            }}
+          />
+        </View>
       </View>
     </Fragment>
   );
@@ -54,10 +79,20 @@ const Assignment = (props) => {
 const styles = StyleSheet.create({
   cardContainerStyle: {
     width: Dimensions.get("window").width * 0.9,
+    maxHeight: "84%",
     minHeight: "50%",
     borderRadius: 50,
     justifyContent: "center",
-    alignItems: "center",
+  },
+  buttonContainer: {
+    padding: 15,
+    flexDirection: "row-reverse",
+    justifyContent: "space-between",
+  },
+  button: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "center",
   },
 });
 
