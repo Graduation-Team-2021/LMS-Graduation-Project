814bd29e674ce54d8d65cadce24e7b3dbf23b7e8
AdhamNour <adhamnourelwaffaa@gmail.com>
2021-07-15 08:10:23 +0200

    finished users and courses search result

--- frontend/frontend-mob/components/CourseListItem.js
+++ frontend/frontend-mob/components/CourseListItem.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { View, StyleSheet, Text, TouchableNativeFeedback } from "react-native";
 import { Avatar } from "react-native-elements";
+import sha256 from "crypto-js/sha512";
 
 const CourseListItem = (props) => {
   const Course = props.Course;
+  const hashedItem = sha256(JSON.stringify(Course));
+  let avatarColor = "#";
+  for (let index = 0; index < 6; index++) {
+    const element = hashedItem.words[index];
+    const newIndex = Math.abs(element % 16);
+    avatarColor = avatarColor.concat(newIndex.toString(16));
+  }
   const navigation = props.navigation;
   return (
     <View style={{ margin: 10 }}>
@@ -18,10 +26,13 @@ const CourseListItem = (props) => {
         <View style={{ flexDirection: "row" }}>
           <Avatar
             rounded
-            size="xlarge"
-            source={{
-              uri: Course.CoursePicture,
+            size="large"
+            containerStyle={{
+              backgroundColor: avatarColor,
+              justifyContent: "center",
             }}
+            title={Course.CourseID.toUpperCase()}
+            titleStyle={{ fontSize: 14 }}
           />
           <View style={{ justifyContent: "center", padding: 15 }}>
             <Text>{Course.CourseName}</Text>
