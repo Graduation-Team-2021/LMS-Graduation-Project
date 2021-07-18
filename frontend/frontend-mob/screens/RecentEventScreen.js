ecab3f69a5735a41015b604114252ef9dfaf9881
AdhamNour <adhamnourelwaffaa@gmail.com>
2021-07-18 05:38:58 +0200

    added new screen

---
+++ frontend/frontend-mob/screens/RecentEventScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+const RecentEventScreen = (props) => {
+  return (
+    <View>
+      <Text>Here is the recent event screen</Text>
+    </View>
+  );
+};
+
+export default RecentEventScreen;
