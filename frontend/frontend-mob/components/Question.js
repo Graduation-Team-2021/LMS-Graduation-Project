cc82f7d4e64b790560acb28c77180da87961656f
Adham Nour <adhamnourelwaffaa@gmail.com>
2021-07-22 03:45:22 +0200

    Quiz Questions are implemented successfully

--- frontend/frontend-mob/components/Question.js
+++ frontend/frontend-mob/components/Question.js
@@ -4,19 +4,19 @@ import { RadioButton } from "react-native-paper";
 
 const Question = (props) => {
   //I expect to get the Question head and the possible answers
-  const [value, setValue] = React.useState("first");
+  const [value, setValue] = useState("first");
 
   return (
     <Fragment>
-      <Text>{props.question}</Text>
+      <Text>{props.question.question}</Text>
       <RadioButton.Group
         onValueChange={(newValue) => setValue(newValue)}
         value={value}
       >
-        {props.answers.map((answer) => (
-          <View style={styles.RB}>
-            <RadioButton value={answer} />
-            <Text>{answer}</Text>
+        {props.question.answers.map((answer, index) => (
+          <View style={styles.RB} key={index}>
+            <RadioButton value={answer.answers} />
+            <Text>{answer.answers}</Text>
           </View>
         ))}
       </RadioButton.Group>
