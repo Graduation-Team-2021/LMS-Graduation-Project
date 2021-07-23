2586964aaf4acb46c43d3104ab1e5166be4f9c32
Adham Nour <adhamnourelwaffaa@gmail.com>
2021-07-23 02:26:35 +0200

    fixed samll bug

--- frontend/frontend-mob/screens/CreateQuizScreen.js
+++ frontend/frontend-mob/screens/CreateQuizScreen.js
@@ -1,6 +1,12 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
-import { TextInput, Button } from "react-native-paper";
+import {
+  TextInput,
+  Button,
+  IconButton,
+  Colors,
+  RadioButton,
+} from "react-native-paper";
 import { useForm, Controller, useFieldArray } from "react-hook-form";
 import { connect } from "react-redux";
 import { mapDispatchToProps, mapStateToProps } from "../store/reduxMaps";
@@ -13,7 +19,7 @@ const CreateQuizScreen = (props) => {
     reset,
     getValues,
     formState: { errors },
-  } = useForm({ defaultValues: { question: "", answers: [{ answer: "" }] } });
+  } = useForm({ defaultValues: { question: "" } });
 
   const { fields, append, prepend, remove, swap, move, insert } = useFieldArray(
     { control, name: "answers" }
@@ -24,15 +30,24 @@ const CreateQuizScreen = (props) => {
     console.log(data);
     console.log("[DATA]====================================");
   };
+  const [value, setValue] = useState("choose");
+
+  useEffect(() => {
+    remove();
+    if (value === "true/false") {
+      append([
+        { answer: "true", correct: false },
+        { answer: "false", correct: false },
+      ]);
+    }
+  }, [value]);
 
   return (
     <View style={styles.main}>
       <Controller
         control={control}
         render={({ field }) => {
-          return (
-            <TextInput label="question" {...field} error={errors.newPassword} />
-          );
+          return <TextInput label="question" {...field} />;
         }}
         name="question"
         defaultValue={""}
@@ -46,31 +61,91 @@ const CreateQuizScreen = (props) => {
           return <Text>Hi</Text>;
         }}
       />
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <View>
+          <Text>Choose Type</Text>
+          <RadioButton.Group
+            onValueChange={(newValue) => setValue(newValue)}
+            value={value}
+          >
+            <View style={{ flexDirection: "row" }}>
+              <RadioButton value="true/false" />
+              <Text>true/false</Text>
+            </View>
+            <View style={{ flexDirection: "row" }}>
+              <RadioButton value="choose" />
+              <Text>choose</Text>
+            </View>
+          </RadioButton.Group>
+        </View>
+        <View style={{ flex: 1, paddingHorizontal: 5 }}>
+          <TextInput mode="outlined" label="score" />
+        </View>
+      </View>
       <ScrollView>
         {fields.map((item, index) => {
           return (
             <Fragment key={item.id}>
-              <Controller
-                key={item.id}
-                control={control}
-                render={({ field: { onBlur, onChange, value } }) => {
-                  return (
-                    <TextInput
-                      label={`answer #${index + 1}`}
-                      error={errors.newPassword}
-                      onBlur={onBlur}
-                      onChange={onChange}
-                      value={value}
-                    />
-                  );
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  padding: 10,
                 }}
-                name={`answers.${index}`}
-                defaultValue={item.value}
-                rules={{ minLength: 1 }}
-              />
+              >
+                <Controller
+                  control={control}
+                  render={({ field: { onBlur, onChange, value } }) => {
+                    return (
+                      <TextInput
+                        label={`answer #${index + 1}`}
+                        onBlur={onBlur}
+                        onChangeText={(text) => onChange(text)}
+                        value={value}
+                        style={{ flex: 1 }}
+                        mode="outlined"
+                      />
+                    );
+                  }}
+                  name={`answers.${index}.answer`}
+                  defaultValue={item.value}
+                  rules={{ minLength: 1, required: true }}
+                />
+
+                <Controller
+                  control={control}
+                  render={({ field: { onBlur, onChange, value } }) => {
+                    return (
+                      <IconButton
+                        icon={value ? "check" : "close"}
+                        color={value ? Colors.green500 : Colors.red700}
+                        size={20}
+                        onPress={() => onChange(!value)}
+                      />
+                    );
+                  }}
+                  name={`answers.${index}.correct`}
+                  defaultValue={item.value}
+                  rules={{ minLength: 1, required: true }}
+                />
+
+                <IconButton
+                  icon="delete"
+                  color={Colors.red400}
+                  size={20}
+                  onPress={() => remove(index)}
+                />
+              </View>
               <ErrorMessage
                 errors={errors}
-                name={`answers.${index}`}
+                name={`answers.${index}.answer`}
                 render={(kak) => {
                   console.log("message", kak);
                   return <Text>Hi</Text>;
@@ -81,18 +156,17 @@ const CreateQuizScreen = (props) => {
         })}
       </ScrollView>
       <View style={styles.buttonPadding}>
-        <Button mode="contained" onPress={() => append({ answer: "" })}>
+        <Button
+          mode="contained"
+          onPress={() => append({ answer: "", correct: false })}
+        >
           add new answer
         </Button>
       </View>
 
       <View style={styles.buttonPadding}>
-        <Button
-          icon="lock-reset"
-          mode="contained"
-          onPress={handleSubmit(onSubmit)}
-        >
-          Reset My Password
+        <Button icon="check" mode="contained" onPress={handleSubmit(onSubmit)}>
+          submit Question
         </Button>
       </View>
     </View>
@@ -102,7 +176,6 @@ const CreateQuizScreen = (props) => {
 const styles = StyleSheet.create({
   main: {
     flex: 1,
-    // backgroundColor: "red",
     justifyContent: "center",
     margin: 0,
     padding: 10,
