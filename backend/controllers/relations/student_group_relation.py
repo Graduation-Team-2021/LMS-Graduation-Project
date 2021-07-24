0053f8105de615b7db114d756a6c537f351361cb
DJ-Man-2099 <davidmesak@hotmail.com>
2021-07-25 01:02:11 +0200

    Get all groups

--- backend/controllers/relations/student_group_relation.py
+++ backend/controllers/relations/student_group_relation.py
@@ -10,14 +10,9 @@ from flask import jsonify
 
 class StudentGroupRelationController:
     def get_one_student_all_groups(self,student_id):
-        groups=Learns_Relation.query.\
-            filter(Learns_Relation.student_id==student_id).join(Course)\
-                .filter(Course.course_code==Learns_Relation.course_code)\
-                        .join(GroupCourseRelation)\
-            .filter(Course.course_code==GroupCourseRelation.course_id).join(GroupProject).\
-                filter(GroupProject.group_id==StudentGroupRelation.group_id).join(StudentGroupRelation)\
-            .filter(StudentGroupRelation.student_id==student_id)\
-                .with_entities(GroupProject.group_name,GroupProject.group_name,GroupCourseRelation.group_id,GroupProject.group_description,GroupProject.post_owner_id, GroupProject.group_pic)
+        groups=StudentGroupRelation.query.join(GroupProject).\
+                filter(GroupProject.group_id==StudentGroupRelation.group_id).filter(StudentGroupRelation.student_id==student_id)\
+                .with_entities(GroupProject.group_name,GroupProject.group_name,GroupProject.group_id,GroupProject.group_description,GroupProject.post_owner_id, GroupProject.group_pic)
         data=[g for g in groups]
         return data
 
