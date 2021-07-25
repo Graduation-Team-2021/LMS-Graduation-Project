4d1d5094aaefba9f2a46a4ea577935bcb3ec41fa
DJ-Man-2099 <davidmesak@hotmail.com>
2021-07-25 12:26:28 +0200

    Done

--- frontend/frontend-web/src/Containers/SearchPage/Modal/Group/Group.js
+++ frontend/frontend-web/src/Containers/SearchPage/Modal/Group/Group.js
@@ -3,7 +3,7 @@ import classes from "./Group.module.css";
 import ImageHolder from "../../../../Components/ImageHolder/ImageHolder";
 import Button from "../../../../Components/Button/Button";
 import { setGroup } from "../../../../Models/Group";
-import {withRouter} from 'react-router-dom'
+import { withRouter } from "react-router-dom";
 
 const Group = (props) => {
   const user = props.Data;
@@ -18,30 +18,34 @@ const Group = (props) => {
         <div>{user.group_description || "No Description"}</div>
       </span>
       <span className={classes.Button}>
-        {user.status==='Enrolled'?<Button
-          className={classes.Inner}
-          onClick={() => {
-            if (user.status === "Enrolled") {
-              props.dismiss();
-              var Group = setGroup(user)
-              props.history.push({
-                pathname: `/group/${user.group_id}`,
-                state:{
-                  isJoined: "true",
-                  Data: setGroup(user),
-                  postID: Group.Post,
-                  name: Group.Title,
-                  Desc: Group.Desc
-                }
-              });
-            } else {
-              //Enroll in Group
-              props.show(true)
-            }
-          }}
-        >
-          {user.status === "Enrolled" ? "Go to Page" : "Enroll"}
-        </Button>:null}
+        {(props.userData.Role === "student" ||
+          props.userData.Role === "professor") &&
+        user.status === "Enrolled" ? (
+          <Button
+            className={classes.Inner}
+            onClick={() => {
+              if (user.status === "Enrolled") {
+                props.dismiss();
+                var Group = setGroup(user);
+                props.history.push({
+                  pathname: `/group/${user.group_id}`,
+                  state: {
+                    isJoined: "true",
+                    Data: setGroup(user),
+                    postID: Group.Post,
+                    name: Group.Title,
+                    Desc: Group.Desc,
+                  },
+                });
+              } else {
+                //Enroll in Group
+                props.show(true);
+              }
+            }}
+          >
+            {user.status === "Enrolled" ? "Go to Page" : "Enroll"}
+          </Button>
+        ) : null}
       </span>
     </div>
   );
